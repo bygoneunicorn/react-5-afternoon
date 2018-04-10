c325180c8291dbf034163cf4599409814212ae79
bygoneunicorn <nathansoccer.jones@gmail.com>
2018-04-10 08:29:11 -0600

    project completed practice1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux';
+import {HashRouter} from 'react-router-dom';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+    <HashRouter>
+        <App />
+    </HashRouter>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
 
